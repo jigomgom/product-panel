a7803c3ad1a60a2a65de2b6470dcc9fd2428c95b
jigomgom <dydrkfl5519@gmail.com>
2025-06-04 16:01:30 +0900

    feat: 프로젝트 구성에 불필요 삭제 및 eslint 규칙 추가

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2, // NOTE: 텝을 눌렀을 때 몇칸이 띄어지는지를 설정합니다.
+  semi: true, // NOTE: 문장 뒤에 세미콜론을 붙힐지 뺄지를 설정합니다.
+  singleQuote: false, // NOTE:  홑따옴표를 쓸건지 설정합니다. 기본값은 쌍따옴표가 기본입니다. 홑따옴표를 썼다면 강제로 쌍따옴표로 변경해 버립니다.
+  bracketSpacing: true, // NOTE: 객체 리터럴 내부의 공백을 제어합니다.
+  endOfLine: "auto", // NOTE: EoF 방식, OS별로 처리 방식이 다릅니다.
+  printWidth: 80, // NOTE: 한줄 내에서 코드를 맞춥니다. 한 줄이 이 글자수를 넘게 되면 줄바꿈되어 코드가 정리됩니다.
+  arrowParens: "always", // NOTE: 단독 화살표 함수의 매개 변수 주위에 괄호를 자동으로 붙힙니다.
+  trailingComma: "all", // NOTE: 객체, 배열, 함수 등의 후행에 쉼표를 찍을지 제어합니다.
+  useTabs: false, // NOTE: 탭 사용 여부를 선택합니다.
+};
